2735197e05428f52a5c98b891e452b2c08fe8dc7
eirikek <eirik.engen.kvam@gmail.com>
2026-05-29 22:03:05 +0200

    Update deployment configuration in sanity.cli.ts to include appId and remove outdated comments.

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -6,10 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
+    appId: 'o6fk9d69skymw0seqfmxnl9n',
     autoUpdates: true,
   }
 })
